e56cf906abbe09d513ebaa0535c77aebea4b6dbf
Mhlengi Ngubane <87973826+MhlengiNgubane@users.noreply.github.com>
2024-03-15 23:53:49 +0200

    food page

--- lib/views/food/food_page.dart
+++ lib/views/food/food_page.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:soeasy/constants/constants.dart';
 import 'package:soeasy/models/foods_model.dart';
 
@@ -19,11 +21,49 @@ class _FoodPageState extends State<FoodPage> {
         physics: const NeverScrollableScrollPhysics(),
         padding: EdgeInsets.zero,
         children: [
-          Container(
-            width: width,
-            height: height,
-            color: kPrimary,
-          )
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(30.r),
+                ),
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      height: 230.h,
+                      child: PageView.builder(
+                          itemCount: widget.food.imageUrl.length,
+                          itemBuilder: (context, i) {
+                            return Container(
+                              width: width,
+                              height: 230.h,
+                              color: kLightWhite,
+                              child: CachedNetworkImage(
+                                  fit: BoxFit.cover,
+                                  imageUrl: widget.food.imageUrl[i]),
+                            );
+                          }),
+                    ),
+                    Positioned(
+                        bottom: 10,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: List.generate(widget.food.imageUrl.length,
+                              (index) {
+                            return Container(
+                              margin: const EdgeInsets.only(left: 12.0),
+                              width: 10.w,
+                              height: 10.h,
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle, color: kSecondary),
+                            );
+                          }),
+                        )),
+                  ],
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
